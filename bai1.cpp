21124c770aafdbe9dc7f4438f1d85155963fcbc9
phongtb24 <80147834+phongtb24@users.noreply.github.com>
2021-03-06 20:12:57 +0700

    commit 1

---
+++ bai1.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+bool checkmate(int a[] , int b[],int n ){
+    for(int i = 0 ; i < n ; i++){
+        for(int j = i + 1 ; j < n ; j++){
+            if(a[i] == a[j]){
+                return true ;
+                break;
+            }
+            if(b[i] == b[j]){
+                return true ;
+                break;
+            }
+            if(abs(a[i] - a[j]) - abs(b[i] - b[j]) == 0){
+                return true ;
+                break;
+            }
+        }
+    }
+    return false ;
+}
+int main(){
+    int n ;
+    cin >> n ;
+    int a[n+1]; 
+    int b[n+1];
+    for(int i = 0 ; i < n ; i++){
+        cin >> a[i] >> b[i];
+    }
+    if(checkmate(a,b,n) == true) {
+        cout <<"yes";
+    }
+    else {
+        cout <<"no";
+    }
+    return 0;
+}
